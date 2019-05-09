39738a87bc0625807d4244f34af782e5cff99117
ToteBrick <402981948@qq.com>
2019-05-09 23:15:55 +0800

    用带参数的装饰器模式模拟Flask的router.并增减logging日志输出。

---
+++ framework.py
@@ -0,0 +1,216 @@
+import time
+import pymysql
+import json
+import logging
+"""处理动态资源请求的框架程序"""
+
+# 路由列表 模仿django方式开发的
+route_list = []
+
+
+
+# 带有参数的装饰器
+def my_route(path):
+    # 装饰器
+    def decorator(fn):
+        # 当装饰器装饰函数的时候就需要给路由列表添加路由
+        route_list.append((path, fn))
+
+        def inner():
+            return fn()
+
+        return inner
+
+    return decorator
+
+# 错误页面
+@my_route('/error.html')
+def error():
+    # open() if encoding is not specified the encoding used is platform dependent
+    with open("template/error.html", "r", encoding='utf-8') as file:
+        file_data = file.read()
+    # 状态
+    status = "404 Not find"
+    # 响应头
+    headers = [("Server", "HJ5.0")]
+
+    # 把处理结果进行返回
+    return status, headers, file_data
+
+# grand页面
+@my_route('/grand.html')
+def error():
+    # open() if encoding is not specified the encoding used is platform dependent
+    with open("template/grand.html", "r", encoding='utf-8') as file:
+        file_data = file.read()
+    # 状态
+    status = "200 OK"
+    # 响应头
+    headers = [("Server", "HJ5.0")]
+
+    # 把处理结果进行返回
+    return status, headers, file_data
+# 获取首页数据
+@my_route("/index.html")
+def index():
+    # 1.读取模板文件数据
+    # open() if encoding is not specified the encoding used is platform dependent
+    with open("template/index.html", "r",encoding='utf-8') as file:
+        file_data = file.read()
+
+    # 2.从数据库查询数据
+    conn = pymysql.connect(host="localhost", port=3306,
+                    user="root", password="mysql",
+                    database="stock_db", charset="utf8")
+
+    # 获取游标
+    cursor = conn.cursor()
+    # 定义sql
+    sql = "select * from info;"
+    # 执行sql
+    cursor.execute(sql)
+    # 获取结果集
+    result = cursor.fetchall()
+    print(result)
+
+    # 遍历每一条数据，把数据封装程一个tr标签把数据放到td里面
+    # 前后端不分离
+    data = ""
+    for row in result:
+        data += '''<tr>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td>%s</td>
+            <td><input type="button" value="添加" id="toAdd" name="toAdd" systemidvaule="000007"></td>
+           </tr>''' % row
+
+    # 关闭游标
+    cursor.close()
+    # 关闭连接
+    conn.close()
+
+    # 3.把查询的数据插入到模板文件中
+
+    # 状态
+    status = "200 OK"
+    # 响应头
+    headers = [("Server", "HJ5.0")]
+    # 这里的data是模板文件数据+数据库里面的数据
+    data = file_data.replace("{%content%}", data)
+    # 把处理结果进行返回
+    return status, headers, data
+
+
+@my_route("/center_data.html")
+def center_data():
+    # 2.从数据库查询数据
+    conn = pymysql.connect(host="localhost", port=3306,
+                           user="root", password="mysql",
+                           database="stock_db", charset="utf8")
+
+    # 获取游标
+    cursor = conn.cursor()
+    try:
+        sql = '''select i.code, i.short, i.chg, 
+                i.turnover, i.price, i.highs, 
+                f.note_info from info as i inner join focus as f 
+                on i.id = f.info_id'''
+        # 执行sql
+        cursor.execute(sql)
+        # 获取结果集
+        result = cursor.fetchall()
+        print(result)
+    except Exception as e:
+        print('错误信息为：', e)
+    finally:
+        cursor.close()
+        conn.close()
+
+    # 个人中心数据列表
+    center_list = list()
+    # 遍历每一条记录，把数据转成字典
+    for row in result:
+        # 把元组转成字典
+        center_dict = dict()
+        center_dict["code"] = row[0]
+        center_dict["short"] = row[1]
+        center_dict["chg"] = row[2]
+        center_dict["turnover"] = row[3]
+        center_dict["price"] = str(row[4])
+        center_dict["highs"] = str(row[5])
+        center_dict["note_info"] = row[6]
+        # 把字典添加到列表
+        center_list.append(center_dict)
+
+
+    # 把列表字典转成json字符串, dumps 不能把Decimal转成json数据
+    # ensure_ascii 表示不使用ascii编码
+    json_str = json.dumps(center_list, ensure_ascii=False)
+
+    print(json_str)
+    # 状态
+    status = "200 OK"
+    # 响应头
+    headers = [("Server", "HJ5.0"), ("Content-Type", "text/html;charset=utf-8")]
+    return status, headers, json_str
+
+
+# 获取个人中心数据
+@my_route("/center.html")  # 1. decorator = my_route("/center.html") 2. @decorator
+# 3. center =  decorator(center)  cenre = inner
+def center():
+    # 1.读取模板文件数据
+    with open("template/center.html", "r",encoding='utf-8') as file:
+        file_data = file.read()
+    # 2.从数据库查询数据
+    # 3.把查询的数据插入到模板文件中
+    data = ""
+    # 状态
+    status = "200 OK"
+    # 响应头
+    headers = [("Server", "HJ5.0")]
+    # 这里的data是模板文件数据+数据库里面的数据
+    data = file_data.replace("{%content%}", data)
+    # 把处理结果进行返回
+    return status, headers, data
+
+
+# 处理动态资源不存在的请求, 此函数已不可用，移交到error.html页面
+def not_found():
+    # 状态
+    status = "404 Not Found"
+    # 响应头
+    headers = [("Server", "HJ5.0")]
+    # 响应头
+    data = "not found"
+    # 把处理结果进行返回
+    return status, headers, data
+
+
+# 处理动态资源请求的函数
+def handle_request(env):
+    # 通过key取值
+    request_path = env["request_path"]
+    print("接收到的动态资源路径是:", request_path)
+
+    # 遍历路由列表，根据请求的动态资源逻辑获取处理的请求函数
+    for path, func in route_list:
+        if request_path == path:
+            # 处理动态资源请求需要的数据
+            result = func()
+            # 把处理后的结果返回给web服务器
+            return result
+    else:
+        # 404,没有找到url对应的处理函数
+        logging.error("没有设置相关路径配置:" + request_path)
+        result = not_found()
+        return result
+
+if __name__ == '__main__':
+    # print(route_list)
+    center_data()
---
+++ web.py
@@ -0,0 +1,190 @@
+import socket
+import threading
+import framework
+import logging
+
+# 配置logging日志信息, 只配置一次，整个应用程序生效
+logging.basicConfig(level=logging.INFO,
+                    format="%(asctime)s-%(filename)s[%(lineno)d]-%(levelname)s:%(message)s",
+                    filename="out.txt",
+                    filemode="a")
+
+
+# 定义web服务器类
+class HttpWebServer(object):
+    def __init__(self, port):
+        # 创建tcp服务端套接字
+        tcp_server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # 设置端口号复用, 程序退出端口立即释放
+        tcp_server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        # 绑定端口号
+        tcp_server_socket.bind(("", port))
+        # 设置监听
+        tcp_server_socket.listen(128)
+        # 保存创建成功的服务器套接字
+        self.tcp_server_socket = tcp_server_socket
+
+    # 处理客户端的请求
+    @staticmethod
+    def handle_client_request(new_socket):
+        # 代码执行到此，说明连接建立成功
+        recv_client_data = new_socket.recv(4096)
+        if len(recv_client_data) == 0:
+            print("关闭浏览器了")
+            new_socket.close()
+            return
+
+        # 对二进制数据进行解码
+        recv_client_content = recv_client_data.decode("utf-8")
+        print(recv_client_content)
+        # 根据指定字符串进行分割， 最大分割次数指定2
+        request_list = recv_client_content.split(" ", maxsplit=2)
+
+        # 获取请求资源路径
+        request_path = request_list[1]
+        print(request_path)
+
+        # 判断请求的是否是根目录，如果条件成立，指定首页数据返回
+        if request_path == "/":
+            request_path = "/index.html"
+
+        # 判断请求的资源路径的后缀是否是.html
+        # 如果是.html那么就是动态资源请求
+        # 如果不是.html那么就任务是静态资源请求
+
+        if request_path.endswith(".html"):
+            """动态资源请求"""
+            logging.info("动态资源请求路径:" + request_path)
+            # 动态资源请求交给web框架处理
+            # 准备给web框架程序的数据, 最主要的是请求路径，后续框架有可能还需要请求头信息
+            env = {
+                "request_path": request_path,
+                # 假如框架需要请求头信息信息，可以在额外增加对应的键值对信息即可
+            }
+            # 处理结果应该返回的信息有:
+            # 1.状态信息status 2. 响应头信息headers 3. 处理结果(响应体)response_data
+            status, headers, data = framework.handle_request(env)
+            stutus_code = status.split(' ')[0] # 打印状态码
+            if stutus_code =='404':  # 判断如果状态码是404，给浏览器返回错误页面error.html
+                with open("template/error.html", "r", encoding='utf-8') as file:
+                    file_data = file.read()
+                    response_header = ""
+                for header in headers:
+                    # header是一个元组，格式化占位符要是有多个，可以使用元组方式进行传参
+                    # “%s %s” % ('ab', 'bc')
+                    response_header += "%s: %s\r\n" % header
+                response_line = "HTTP/1.1 %s\r\n" % status
+                # web服务器把处理后的结果拼接程一个http的响应报文发送给浏览器
+                # web服务器把处理后的结果拼接程一个http的响应报文发送给浏览器
+                response_data = (response_line + response_header +
+                                 "\r\n" + file_data).encode("utf-8")
+                # 把服务器发送的数据发给浏览器
+                new_socket.send(response_data)
+                # 关闭套接字
+                new_socket.close()
+            else:
+                # 响应行
+                response_line = "HTTP/1.1 %s\r\n" % status
+                # 遍历响应头信息[("Server", "HJ5.0")]
+                response_header = ""
+                for header in headers:
+                    # header是一个元组，格式化占位符要是有多个，可以使用元组方式进行传参
+                    # “%s %s” % ('ab', 'bc')
+                    response_header += "%s: %s\r\n" % header
+
+                # web服务器把处理后的结果拼接程一个http的响应报文发送给浏览器
+                response_data = (response_line + response_header +
+                                 "\r\n" + data).encode("utf-8")
+
+                # 把服务器发送的数据发给浏览器
+                new_socket.send(response_data)
+                # 关闭套接字
+                new_socket.close()
+
+        else:
+            """静态资源请求"""
+            logging.info("静态资源请求路径:" + request_path)
+            try:
+                # 动态打开指定文件
+                with open("static" + request_path, "rb") as file:
+                    # 读取文件数据
+                    file_data = file.read()
+            except Exception as e:
+                logging.warning("没有请求的资源文件(非html):" + str(e))
+                env = {
+                    "request_path": request_path,
+                    # 假如框架需要请求头信息信息，可以在额外增加对应的键值对信息即可
+                }
+                status, headers, data = framework.handle_request(env)
+                with open("template/error.html", "r", encoding='utf-8') as file:
+                    file_data = file.read()
+                    response_header = ""
+                for header in headers:
+                    # header是一个元组，格式化占位符要是有多个，可以使用元组方式进行传参
+                    # “%s %s” % ('ab', 'bc')
+                    response_header += "%s: %s\r\n" % header
+                response_line = "HTTP/1.1 %s\r\n" % status
+                # web服务器把处理后的结果拼接程一个http的响应报文发送给浏览器
+                # web服务器把处理后的结果拼接程一个http的响应报文发送给浏览器
+                response_data = (response_line + response_header +
+                                 "\r\n" + file_data).encode("utf-8")
+                # 把服务器发送的数据发给浏览器
+                new_socket.send(response_data)
+                # 关闭套接字
+                new_socket.close()
+            else:
+                # 响应行
+                response_line = "HTTP/1.1 200 OK\r\n"
+                # 响应头
+                response_header = "Server: HJ5.0\r\n"
+
+                # 响应体
+                response_body = file_data
+
+                # 拼接响应报文
+                response_data = (response_line + response_header + "\r\n").encode("utf-8") + response_body
+                # 发送数据
+                new_socket.send(response_data)
+            finally:
+                # 关闭服务与客户端的套接字
+                new_socket.close()
+
+    # 启动web服务器进行工作
+    def start(self):
+        while True:
+            # 等待接受客户端的连接请求
+            new_socket, ip_port = self.tcp_server_socket.accept()
+            # 当客户端和服务器建立连接程，创建子线程
+            sub_thread = threading.Thread(target=self.handle_client_request, args=(new_socket,))
+            # 设置守护主线程
+            sub_thread.setDaemon(True)
+            # 启动子线程执行对应的任务
+            sub_thread.start()
+
+
+# 程序入口函数
+def main():
+
+    # print(sys.argv)
+    # 判断命令行参数是否等于2,
+    # if len(sys.argv) != 2:
+    #     logging.warning("用户在终端启动程序参数个数不正确")
+    #     print("执行命令如下: python3 xxx.py 8000")
+    #     return
+    #
+    # # 判断字符串是否都是数字组成
+    # if not sys.argv[1].isdigit():
+    #     logging.warning("用户在终端启动程序参数类型不正确")
+    #     print("执行命令如下: python3 xxx.py 8000")
+    #     return
+
+    # 获取终端命令行参数
+    # port = int(sys.argv[1])
+    # 创建web服务器对象
+    web_server = HttpWebServer(8585)
+    # 启动web服务器进行工作
+    web_server.start()
+
+
+if __name__ == '__main__':
+    main()
